a2a055ef6920097c69df187e43c6d23f397343ed
M-Keet <m.keet@student.fontys.nl>
2017-10-09 15:54:48 +0200

    Header maken en footer beginnen

--- includes/header.php
+++ includes/header.php
@@ -5,3 +5,24 @@
 	<link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
+	<header>
+		<div class="container">
+			<img src="images/logo.png" alt="Logo T.O.M. BV">
+			<nav>
+				<ul>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">T.O.M.</a>
+					</li>
+					<li>
+						<a href="#">Scorebord</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
