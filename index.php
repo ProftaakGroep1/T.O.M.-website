07bc1a903a5e25a2547e49f7be2cee98bee34c6c
M-Keet <m.keet@student.fontys.nl>
2017-10-21 21:51:45 +0200

    Koppen aan homeBanner toevoegen
    Overlap in achtergrond moet nog opgelost worden

--- index.php
+++ index.php
@@ -4,7 +4,14 @@ include "includes/header.php";
 ?>
 <!-- </header> -->
 <section class="title">
-	
+	<div class="bannerContainer">
+		<h1>
+			<span>Maak topo leren leuk!</span>
+		</h1>
+		<h2>
+			<span>Met de Topografie Onderwijs Machine</span>
+		</h2>
+	</div>
 </section>
 <main>
 	
