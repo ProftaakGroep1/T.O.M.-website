d7f0e0bf3a58f10133d3b0c695a5f618263627c9
M-Keet <m.keet@student.fontys.nl>
2017-10-22 00:13:40 +0200

    Samenvatting en T.O.M.  aan homepagina toegevoegd

--- index.php
+++ index.php
@@ -14,7 +14,18 @@ include "includes/header.php";
 	</div>
 </section>
 <main>
-	
+	<section class="samenvatting">
+		<p>
+			T.O.M. maakt topo leren leuk door spelletjes te spelen met de leerlingen van groep 6. Laat ze achter hun schoolbanken vandaan komen.
+		</p>
+	</section>
+	<section class="tom">
+		<h1>T.O.M.</h1>
+		<img src="images/tom.png" alt="Foto van T.O.M.">
+		<p>
+			T.O.M. is een robot waarmee leerlingen van groep 6 topografie kunnen leren. T.O.M. wordt gebruikt op een mat met daarop de kaart van Nederland. T.O.M. geeft de leerling een opdracht en door de robot te besturen kan die opdracht voltooid worden. Als dit gebeurd is, scoort de leerling punten. 
+		</p>
+	</section>
 </main>
 <!-- <footer> -->
 <?php include "includes/footer.html"; ?>
