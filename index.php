cbd4689f70e12ee5b60e8f802b5d365befc7d3dd
M-Keet <m.keet@student.fontys.nl>
2017-10-22 19:59:20 +0200

    Score sectie toegevoegd

--- index.php
+++ index.php
@@ -19,13 +19,24 @@ include "includes/header.php";
 			T.O.M. maakt topo leren leuk door spelletjes te spelen met de leerlingen van groep 6. Laat ze achter hun schoolbanken vandaan komen.
 		</p>
 	</section>
-	<section class="tom">
-		<h1>T.O.M.</h1>
-		<img src="images/tom.png" alt="Foto van T.O.M.">
-		<p>
-			T.O.M. is een robot waarmee leerlingen van groep 6 topografie kunnen leren. T.O.M. wordt gebruikt op een mat met daarop de kaart van Nederland. T.O.M. geeft de leerling een opdracht en door de robot te besturen kan die opdracht voltooid worden. Als dit gebeurd is, scoort de leerling punten. 
-		</p>
-	</section>
+	<a href="#">
+		<section class="tom secties">
+			<h1>T.O.M.</h1>
+			<img src="images/tom.png" alt="Foto van T.O.M.">
+			<p>
+				T.O.M. is een robot waarmee leerlingen van groep 6 topografie kunnen leren. T.O.M. wordt gebruikt op een mat met daarop de kaart van Nederland. T.O.M. geeft de leerling een opdracht en door de robot te besturen kan die opdracht voltooid worden. Als dit gebeurd is, scoort de leerling punten. 
+			</p>
+		</section>
+	</a>
+	<a href="#">
+		<section class="scores secties">
+			<h1>Scores</h1>
+			<p>
+				De punten die de leerling scoort, worden bijgehouden en door ons verzameld. Hierdoor zie je hoe je ervoor staat ten opzichte van anderen. <br>
+				Hieronder een samenvatting en de laatste veranderingen. 
+			</p>
+		</section>
+	</a>
 </main>
 <!-- <footer> -->
 <?php include "includes/footer.html"; ?>
